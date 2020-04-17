abc470e5542b550ba6a593d5ae612862dc6c4619
alfml <ronaldmacedo@gmail.com>
2020-04-16 22:10:20 -0500

    'request_from_csr' modified to receive an optional 'response_type' parameter

--- lib/blobfish/ejbca/client.rb
+++ lib/blobfish/ejbca/client.rb
@@ -12,6 +12,7 @@ module Blobfish
       TOKEN_TYPE_P12 = 'P12'
       TOKEN_TYPE_USERGENERATED = 'USERGENERATED'
       RESPONSETYPE_CERTIFICATE = 'CERTIFICATE'
+      RESPONSETYPE_PKCS7WITHCHAIN = 'PKCS7WITHCHAIN'
       REVOCATION_REASON_UNSPECIFIED = 0
 
       # @param [String] ws_additional_trusted_anchors e.g. +ca-certificates.crt+. Required only if +wsdl_url+ uses a non-commercial SSL certificate, otherwise it should be +nil+.
@@ -60,15 +61,21 @@ module Blobfish
 
       end
 
-      def request_from_csr(pem_csr, ejbca_username, email_address, subject_dn, subject_alt_name, validity_type, validity_value)
+      def request_from_csr(pem_csr, ejbca_username, email_address, subject_dn, subject_alt_name, validity_type, validity_value, response_type = RESPONSETYPE_CERTIFICATE)
         end_user = create_end_user(ejbca_username, nil, TOKEN_TYPE_USERGENERATED, email_address, subject_dn, subject_alt_name, validity_type, validity_value)
         ws_resp = ws_call(:certificate_request,
                           arg0: end_user,
                           arg1: pem_csr,
                           arg2: 0,
-                          arg4: RESPONSETYPE_CERTIFICATE)
-        cert_as_der = Client.double_decode64(ws_resp[:data])
-        Certificate.new(cert_as_der)
+                          arg4: response_type)
+        resp_as_der = Client.double_decode64(ws_resp[:data])
+        if response_type == RESPONSETYPE_CERTIFICATE
+          Certificate.new(resp_as_der)
+        elsif response_type == RESPONSETYPE_PKCS7WITHCHAIN
+          OpenSSL::PKCS7.new(resp_as_der)
+        else
+          raise NotImplementedError
+        end
       end
 
       def revoke_cert(serial_number)
